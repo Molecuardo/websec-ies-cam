7023ff526203152e5882c774b006d8e742fad263
Pablo <pablodiazpelayo@gmail.com>
2025-10-06 16:29:44 +0200

    mas datos y analisis de ntp

---
+++ analizar_resultados.py
@@ -0,0 +1,51 @@
+import os
+import json
+import random as rd
+
+
+def escanear_carpeta(path):
+    all_files_list = os.listdir(path)
+    if path == "EscaneosNTP/":
+        # de aqui cogemos delta sg nada mas
+        total_delta_sg = 0
+        veces = 0
+        damn = 0
+        num_datos_archivo = 0
+        rand_file = rd.choice(all_files_list)
+        with open(path + rand_file, "r") as rf:
+            json_rand_file = json.load(rf)
+            print(f"Instituto elegido: {json_rand_file['url']}")
+        for i in all_files_list:
+            veces += 1
+            try:
+                with open(path + i, "r") as f:
+                    dict_archivo = json.load(f)
+                    total_delta_sg += dict_archivo["delta_sg"]
+                    """
+                    if dict_archivo["delta_sg"] >= 3600 * 2:
+                        damn += 1
+                        print(dict_archivo["url"])
+                    """
+                    try:
+                        if dict_archivo["url"] == json_rand_file["url"]:
+                            print(dict_archivo["delta_sg"])
+                            num_datos_archivo += 1
+                    except:
+                        pass
+
+            except json.JSONDecodeError:
+                print(f"numero de datos del Instituto: {num_datos_archivo}")
+                print(f"media de delta sg: {total_delta_sg / veces}")
+                print(f"total damns: {damn}")
+                print("Hemos llegado al final. adios")
+                break
+    elif path == "EscaneosCertificados/":
+        pass
+    elif path == "EscaneosLighthouse/":
+        # analizar escaneos lighthouse
+        pass
+    elif path == "EscaneosWebHeader/":
+        pass
+
+
+escanear_carpeta("EscaneosNTP/")
