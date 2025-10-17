1409b5590550e259c859f95ff61cdaf0c259e970
Pablo <pablodiazpelayo@gmail.com>
2025-10-17 17:52:02 +0200

    mas datos y analisis empezado de certificados

--- analizar_resultados.py
+++ analizar_resultados.py
@@ -8,8 +8,9 @@ def escanear_carpeta(path):
     if path == "EscaneosNTP/":
         # de aqui cogemos delta sg nada mas
         total_delta_sg = 0
+        total_delta_sg_instituto = 0
         veces = 0
-        damn = 0
+        #      damn = 0
         num_datos_archivo = 0
         rand_file = rd.choice(all_files_list)
         with open(path + rand_file, "r") as rf:
@@ -20,27 +21,36 @@ def escanear_carpeta(path):
             try:
                 with open(path + i, "r") as f:
                     dict_archivo = json.load(f)
-                    total_delta_sg += dict_archivo["delta_sg"]
-                    """
-                    if dict_archivo["delta_sg"] >= 3600 * 2:
-                        damn += 1
-                        print(dict_archivo["url"])
-                    """
-                    try:
-                        if dict_archivo["url"] == json_rand_file["url"]:
-                            print(dict_archivo["delta_sg"])
-                            num_datos_archivo += 1
-                    except:
-                        pass
+                if dict_archivo["delta_sg"] <= 7000:
+                    total_delta_sg += abs(dict_archivo["delta_sg"])
+                """
+                if dict_archivo["delta_sg"] >= 3600 * 2:
+                    damn += 1
+                    print(dict_archivo["url"])
+                """
+                try:
+                    if dict_archivo["url"] == json_rand_file["url"]:
+                        print(dict_archivo["delta_sg"])
+                        total_delta_sg_instituto += abs(dict_archivo["delta_sg"])
+                        num_datos_archivo += 1
+                except Exception:
+                    pass
 
             except json.JSONDecodeError:
                 print(f"numero de datos del Instituto: {num_datos_archivo}")
-                print(f"media de delta sg: {total_delta_sg / veces}")
-                print(f"total damns: {damn}")
+                print(
+                    f"media de delta sg del ies: {total_delta_sg_instituto / num_datos_archivo}"
+                )
+                #               print(f"total damns: {damn}")
                 print("Hemos llegado al final. adios")
                 break
     elif path == "EscaneosCertificados/":
-        pass
+        # analizamos escaneos de certificados # peak habilidades de comentario
+        for f in all_files_list:
+            with open(path + f, "r") as buf_file:
+                dict_archivo = json.load(buf_file)
+            if dict_archivo[""]
+
     elif path == "EscaneosLighthouse/":
         # analizar escaneos lighthouse
         pass
