b32bbc6a619c83e2ef04b244d1210a7be2eb2561
Pablo <pablodiazpelayo@gmail.com>
2025-11-16 09:39:57 +0100

    mas datos y analisis de certificados

--- analizar_resultados.py
+++ analizar_resultados.py
@@ -1,5 +1,7 @@
 import os
 import json
+
+# import csv ??
 import random as rd
 
 
@@ -7,6 +9,7 @@ def escanear_carpeta(path):
     all_files_list = os.listdir(path)
     if path == "EscaneosNTP/":
         # de aqui cogemos delta sg nada mas
+        # calificacion = 0
         total_delta_sg = 0
         total_delta_sg_instituto = 0
         veces = 0
@@ -21,13 +24,17 @@ def escanear_carpeta(path):
             try:
                 with open(path + i, "r") as f:
                     dict_archivo = json.load(f)
+                    # por si ha usado hora GMT:
                 if dict_archivo["delta_sg"] <= 7000:
                     total_delta_sg += abs(dict_archivo["delta_sg"])
-                """
-                if dict_archivo["delta_sg"] >= 3600 * 2:
-                    damn += 1
-                    print(dict_archivo["url"])
-                """
+                    """
+                    if dict_archivo["delta_sg"] > 60:
+                        calificacion = 0
+                    elif dict_archivo["delta_sg"] > 30:
+                        calificacion = 5
+                    elif dict_archivo["delta_sg"] > 30:
+                        calificacion = 10
+                    """
                 try:
                     if dict_archivo["url"] == json_rand_file["url"]:
                         print(dict_archivo["delta_sg"])
@@ -42,20 +49,47 @@ def escanear_carpeta(path):
                     f"media de delta sg del ies: {total_delta_sg_instituto / num_datos_archivo}"
                 )
                 #               print(f"total damns: {damn}")
-                print("Hemos llegado al final. adios")
+                # TODO: añadir funcionalidad para guardar los resultados en un csv, usando calificacion
+                """
+                with open("resultados_finales_ntp.csv", 'w') as r:
+                  # xd
+                """
                 break
     elif path == "EscaneosCertificados/":
-        # analizamos escaneos de certificados # peak habilidades de comentario
-        for f in all_files_list:
-            with open(path + f, "r") as buf_file:
-                dict_archivo = json.load(buf_file)
-            if dict_archivo[""]
+        # analizamos certificados
+        #       calificacion = 0
+        suma_calificaciones = 0
+        veces = 0
+        for j in all_files_list:
+            try:
+                with open(path + j, "r") as f:
+                    dict_archivo = json.load(f)
+                veces += 1
+                if dict_archivo["calidad"] == "Bueno":
+                    suma_calificaciones += 10
+                elif dict_archivo["calidad"] == "Aceptable":
+                    suma_calificaciones += 5
+                elif dict_archivo["calidad"] == "Deficiente":
+                    suma_calificaciones += 0
+
+            except json.JSONDecodeError:
+                print(
+                    f"calificacion media de certificados: {suma_calificaciones / veces}"
+                )
+                break
+        else:
+            print(f"calificacion media de certificados: {suma_calificaciones / veces}")
+        # TODO: añadir funcionalidad para guardar los resultados en un csv, usando calificacion
 
     elif path == "EscaneosLighthouse/":
-        # analizar escaneos lighthouse
+        # analizar Lighthouse
         pass
     elif path == "EscaneosWebHeader/":
+        # analizar Cabeceras
         pass
+    else:
+        raise Exception("No hay carpetas con ese nombre")
 
 
 escanear_carpeta("EscaneosNTP/")
+escanear_carpeta("EscaneosCertificados/")
